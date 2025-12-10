804bf1aebcb653c288b08d8678be4c32c17e3bf3
Won Chan LEE <gxpk2978@naver.com>
2025-12-10 23:01:53 +0900

    feat : map수정 및 벽 한정 충돌 설정

--- components/map/MapScene.ts
+++ components/map/MapScene.ts
@@ -11,8 +11,25 @@ export default class MapScene extends Phaser.Scene {
 
   preload() {
     // 맵 리소스
-    this.load.image("CommonTile", "/tilesets/CommonTile.png");
-    this.load.tilemapTiledJSON("map", "/maps/DanMap8.tmj");
+    this.load.image("CommonTile", "/tilesets/CommonTile.png");      
+    this.load.image("Plants", "/tilesets/Plants.png");      
+    this.load.image("arcade1", "/tilesets/arcade1.png");
+    this.load.image("arcade2", "/tilesets/arcade2.png");
+    this.load.image("BlueChair", "/tilesets/BlueChair.png");
+    this.load.image("button", "/tilesets/button.png");
+    this.load.image("button2", "/tilesets/button2.png");
+    this.load.image("desk1", "/tilesets/desk1.png");
+    this.load.image("desk2", "/tilesets/desk2.png");
+    this.load.image("electronic", "/tilesets/electronic.png");
+    this.load.image("mainDesk", "/tilesets/mainDesk.png");
+    this.load.image("RedChair", "/tilesets/RedChair.png");
+    this.load.image("storefrontSign", "/tilesets/storefrontSign.png");
+    this.load.image("userButton", "/tilesets/userButton.png");
+    this.load.tilemapTiledJSON("map", "/maps/DanArcadeLast1.tmj");
+
+    this.load.image("bg1_1", "/tilesets/bg1_1.png");
+
+
 
   // Body (teen / black)
   this.load.spritesheet(
@@ -38,15 +55,54 @@ export default class MapScene extends Phaser.Scene {
 
   create() {
     const map = this.make.tilemap({ key: "map" });
-    const tileset = map.addTilesetImage("CommonTile", "CommonTile");
 
-      console.log("BODY EXISTS:", this.textures.exists("body_black"));
-      console.log("HAIR EXISTS:", this.textures.exists("hair_black"));
-      console.log("EYES EXISTS:", this.textures.exists("eyes_default"));
+    this.add.image(0, 0, "bg1_1")
+    .setOrigin(0, 0)
+    .setDepth(-1);
 
+        // Tiled tilesets[].name 과 일치하는 이름으로 addTilesetImage
+    const common = map.addTilesetImage("CommonTile", "CommonTile");
+    const mainDesk = map.addTilesetImage("mainDesk", "mainDesk");
+    const desk2 = map.addTilesetImage("desk2", "desk2");
+    const desk1 = map.addTilesetImage("desk1", "desk1");
+    const arcade1 = map.addTilesetImage("arcade1", "arcade1");
+    const arcade2 = map.addTilesetImage("arcade2", "arcade2");
+    const blueChair = map.addTilesetImage("BlueChair", "BlueChair");
+    const redChair = map.addTilesetImage("RedChair", "RedChair");
+    const plants = map.addTilesetImage("Plants", "Plants");
+    const button = map.addTilesetImage("button", "button");
+    const button2 = map.addTilesetImage("button2", "button2");
+    const storefrontSign = map.addTilesetImage("storefrontSign", "storefrontSign");
+    const electronic = map.addTilesetImage("electronic", "electronic");
+    const userButton = map.addTilesetImage("userButton", "userButton");
+
+    const tilesetsRaw = [
+      common,
+      mainDesk,
+      desk2,
+      desk1,
+      arcade1,
+      arcade2,
+      blueChair,
+      redChair,
+      plants,
+      button,
+      button2,
+      storefrontSign,
+      electronic,
+      userButton
+      
+    ];
+
+    const tilesets = tilesetsRaw.filter((ts): ts is Phaser.Tilemaps.Tileset => ts !== null);
+
+
+    this.add.image(0, 0, "bg1_1").setOrigin(0, 0);
     // ground, walls 레이어만 생성
-    map.createLayer("ground", tileset ?? [], 0, 0);
-    const wallsLayer = map.createLayer("walls", tileset?? [], 0, 0);
+    map.createLayer("ground", tilesets , 0, 0);
+    const wallsLayer = map.createLayer("walls", tilesets, 0, 0);
+    map.createLayer("object1", tilesets, 0, 0);
+    map.createLayer("boject2", tilesets, 0, 0); // 오타지만 Tiled name이 boject2니까 그대로
   
     this.physics.world.createDebugGraphic();
 
@@ -60,9 +116,7 @@ export default class MapScene extends Phaser.Scene {
     this.player.setPart("body", "body_black");
     this.player.setPart("hair", "hair_black");
     this.player.setPart("eyes", "eyes_blue");
-
     
-
     // 3) 카메라 따라가기
     this.cameras.main.startFollow(this.player);
     this.cameras.main.setZoom(2);
@@ -72,4 +126,5 @@ export default class MapScene extends Phaser.Scene {
   update() {
     this.player.update();
   }
+
 }
