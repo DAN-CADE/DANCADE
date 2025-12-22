24ff38cc51b0aa58debd81e71b68205eed9ee896
Won Chan LEE <gxpk2978@naver.com>
2025-12-22 14:19:10 +0900

    feat : shop frame 작성

---
+++ app/shop/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+import Window from "@/components/common/Window";
+
+export default function ShopPage(){
+
+
+return(
+  <>
+    <main className="login-page relative min-h-screen">
+      <div className="absolute inset-0 bg-[url('/assets/background/common.png')] bg-cover bg-center bg-no-repeat opacity-15 -z-10" />
+      <Window title="shop">
+
+      <p>???</p>
+      </Window>
+
+
+
+
+    </main>
+
+  </>
+)
+
+
+}
