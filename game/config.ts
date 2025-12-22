7580cac8a409e5b15802da30a17801c8a029c69b
김민중 <f1minn2r@naver.com>
2025-12-22 11:04:45 +0900

    refactor: 게임 화면 반응형 레이아웃 적용 및 배경색 수정

--- game/config.ts
+++ game/config.ts
@@ -6,15 +6,19 @@ export const createGameConfig = (
 ): Phaser.Types.Core.GameConfig => ({
   type: Phaser.AUTO,
   parent: "game-container",
-  width: 800,
-  height: 600,
-  backgroundColor: "#87CEEB",
+  // width: 800,
+  // height: 600,
+  backgroundColor: "#2c2c2c",
   physics: {
     default: "arcade",
     arcade: {
       gravity: { y: 0, x: 0 },
       debug: false,
     },
+  },
+  scale: {
+    mode: Phaser.Scale.ScaleModes.RESIZE,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
   },
   scene: [],
 });
